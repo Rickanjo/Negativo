e9d05a69af6de5538ad33d31a48325a2ead3ded9
Rickanjo <Ricardo.cassettari07@gmail.com>
2023-04-22 14:15:48 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// Leia um número inteiro e exiba a mensagem "Você digitou um número negativo." caso o número seja menor que zero.
+Console.WriteLine("--- Negativo ---\n");
+
+Console.Write("Digite um número inteiro:");
+int numero = int.Parse(Console.ReadLine()!);
+
+if (numero < 0) {
+    Console.WriteLine("Você digitou um número negativo.");
+}
